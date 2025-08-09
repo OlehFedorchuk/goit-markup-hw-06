b884b29c6450dc1c46603e2e81a56e2cefd3ef69
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2025-08-09 15:45:44 +0100

    update

--- js/modal.js
+++ js/modal.js
@@ -1,25 +1,3 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-modal-open]'),
-//     closeModalBtn: document.querySelector('[data-modal-close]'),
-//     modal: document.querySelector('[data-modal]'),
-//   };
-
-//   refs.openModalBtn.addEventListener('click', openModal);
-//   refs.closeModalBtn.addEventListener('click', closeModal);
-
-//   function openModal() {
-//     refs.modal.classList.add('is-open');
-//     document.body.classList.add('no-scroll');
-//     document.documentElement.classList.add('no-scroll');
-//   }
-
-//   function closeModal() {
-//     refs.modal.classList.remove('is-open');
-//     document.body.classList.remove('no-scroll');
-//     document.documentElement.classList.remove('no-scroll');
-//   }
-// })();
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
