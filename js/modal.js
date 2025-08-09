563bbbdfb902d0e6f9522970795fa1d908b37a3e
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2025-08-09 15:44:48 +0100

    hm6 done

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,43 @@
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', openModal);
+//   refs.closeModalBtn.addEventListener('click', closeModal);
+
+//   function openModal() {
+//     refs.modal.classList.add('is-open');
+//     document.body.classList.add('no-scroll');
+//     document.documentElement.classList.add('no-scroll');
+//   }
+
+//   function closeModal() {
+//     refs.modal.classList.remove('is-open');
+//     document.body.classList.remove('no-scroll');
+//     document.documentElement.classList.remove('no-scroll');
+//   }
+// })();
 (() => {
   const refs = {
-    // Додати атрибут data-modal-open на кнопку відкриття
     openModalBtn: document.querySelector('[data-modal-open]'),
-    // Додати атрибут data-modal-close на кнопку закриття
     closeModalBtn: document.querySelector('[data-modal-close]'),
-    // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector('[data-modal]'),
   };
 
+  function toggleModal() {
+    const isOpen = refs.modal.classList.toggle('is-open');
+    if (isOpen) {
+      document.body.classList.add('no-scroll');
+      document.documentElement.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+      document.documentElement.classList.remove('no-scroll');
+    }
+  }
+
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.modal.classList.toggle('is-open');
-  }
 })();
